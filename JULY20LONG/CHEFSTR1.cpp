9b6bde17f4596d0b4b8858573917f2796d447ce9
Basukinath Tiwari <basukinathtiwari231198@gmail.com>
2020-07-03 21:58:23 +0530

    Create CHEFSTR1.cpp

---
+++ JULY20LONG/CHEFSTR1.cpp
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+  public static void main (String[] args) throws java.lang.Exception
+  {
+      Scanner sc = new Scanner(System.in);
+    int x = 1;
+      x = sc.nextInt();
+      while(x > 0) { 
+          
+          int n = sc.nextInt();
+          long cnt = 0;
+          int[] a = new int[n];
+          for(int i = 0; i < n; i++){
+              a[i] = sc.nextInt();
+              if(i > 0) { cnt += Math.abs(a[i] - a[i-1]) - 1;
+                 // System.out.println(a[i] - a[i-1] - 1);
+              }
+              }
+          System.out.println(cnt);
+          x--;
+      }
+  }
+}
